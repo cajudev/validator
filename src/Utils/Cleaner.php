bbe9da4f0b0b98c37087e16415b4635b26738bb3
lopesrichard <contato.richardlopes@outlook.com>
2018-12-22 13:28:39 -0200

    Validação de Rg

--- src/Utils/Cleaner.php
+++ src/Utils/Cleaner.php
@@ -2,15 +2,24 @@
 
 /**
  *
- * Aplica máscara em números
+ * Remove caracteres especiais
  * 
  *  @author Richard Lopes
  */
 
 class Cleaner {
 
-    public static function cleanNumber(&$number) {
-        $number = preg_replace("/[^0-9]/", "", $number);
+    public static function cleanNumber(&$number, $exceptions = '') {
+        $regex  = "/[^0-9";
+
+        //Escapes special characters
+        foreach(str_split($exceptions) as $exception){
+            $regex .= $exception == "/" || $exception == "\\" ? "\\" . $exception : $exception;
+        }
+
+        $regex .= "]/";
+
+        $number = preg_replace($regex, "", $number);
     }
 }
 
